3efec0ebd856d9b969e7e720bddd47ad56ecafa9
Martine Gagne <martine.e.gagne@gmail.com>
2017-07-12 18:28:47 -0400

    create link to Contacts page

--- crm.rb
+++ crm.rb
@@ -2,5 +2,15 @@ require_relative 'contact'
 require 'sinatra'
 
 get '/' do
+  @contact_link = "localhost:4567/contacts"
   erb :index
+end
+
+get '/contacts' do
+  @contacts = Contact.all
+  erb :contacts
+end
+
+after do
+  ActiveRecord::Base.connection.close
 end
