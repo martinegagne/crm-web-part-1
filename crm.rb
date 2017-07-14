ef7bc94bf0ce26780c59894c74aab5261aab5407
Martine Gagne <martine.e.gagne@gmail.com>
2017-07-13 23:14:23 -0400

    add dynamic routes to contact pages

--- crm.rb
+++ crm.rb
@@ -2,14 +2,32 @@ require_relative 'contact'
 require 'sinatra'
 
 get '/' do
-  redirect to ('/contacts')
+  redirect to '/contacts'
   erb :index
 end
 
 get '/contacts' do
+  @landing_page = '/'
   @contacts = Contact.all
 
   erb :contacts
+end
+
+get '/about' do
+
+  erb :about
+end
+
+get '/contacts/:id' do
+  @contact = Contact.find_by({id: params[:id].to_i})
+
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+
+  erb :show_contact
 end
 
 after do
